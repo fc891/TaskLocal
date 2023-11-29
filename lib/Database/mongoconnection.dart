bc19dcbd2137a25afab6f22e4b84c18387ccca0e
Richard Nguyen <81831944+richardn257@users.noreply.github.com>
2023-11-29 01:17:52 -0800

    Updated some files
    Updated the URL in the constants file. Added code for inspecting the connection. Added entitlement in macOS to access network

--- lib/Database/mongoconnection.dart
+++ lib/Database/mongoconnection.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'constants.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
@@ -7,6 +8,7 @@ class MongoConnection {
   static connect() async {
     db = await Db.create(MONGO_CONNECTION_URL);
     await db.open();
+    inspect(db);
     userCollection = db.collection(MONGO_TEST_COLLECTION);
   }
 }
