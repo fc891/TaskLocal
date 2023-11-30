748f215dbd37a9a8f239aef6d4e797db61671bc6
fc891 <76578506+fc891@users.noreply.github.com>
2023-11-30 12:11:16 -0800

    Adding to database
    Added database insert to mongoconnection.dart file

--- lib/Database/mongoconnection.dart
+++ lib/Database/mongoconnection.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'constants.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
@@ -5,8 +6,20 @@ class MongoConnection {
   static var db, userCollection;
 
   static connect() async {
-    db = await Db.create(MONGO_CONNECTION_URL);
+    var db = await Db.create(MONGO_CONNECTION_URL);
     await db.open();
-    userCollection = db.collection(MONGO_TEST_COLLECTION);
+    inspect(db);
+
+    var status = db.serverStatus();
+    print(status);
+
+    var userCollection = db.collection(MONGO_TEST_COLLECTION);
+    await userCollection.insertOne({
+      "username": "billtest",
+      "name": "Bill",
+      "email": "billtest@tasklocal.com"
+    });
+
+    print(await userCollection.find().toList());
   }
 }
