117880e016025dd6b74b86d3aa69290b8e57d52b
Richard Nguyen <81831944+richardn257@users.noreply.github.com>
2023-11-30 11:35:02 -0800

    Create insert.dart

---
+++ lib/insert.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MongoDbInsert extends StatefulWidget {
+  MongoDbInsert({Key?key}) : super(key:key);
+
+  @override
+  _MongoDbInsertState createState() => _MongoDbInsertState();
+}
+
+class _MongoDbInsertState extends State<MongoDbInsert> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
