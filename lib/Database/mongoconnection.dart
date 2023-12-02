60258dd33559e60a5c937d1552462f5c863067b8
Richard Nguyen <81831944+richardn257@users.noreply.github.com>
2023-12-01 23:24:02 -0800

    Update mongoconnection.dart

--- lib/Database/mongoconnection.dart
+++ lib/Database/mongoconnection.dart
@@ -8,21 +8,9 @@ class MongoConnection {
   static var db, userCollection;
 
   static connect() async {
-    var db = await Db.create(MONGO_CONNECTION_URL);
+    db = await Db.create(MONGO_CONNECTION_URL);
     await db.open();
     inspect(db);
-    
-    var status = db.serverStatus();
-    print(status);
-
-    var userCollection = db.collection(MONGO_TEST_COLLECTION);
-    await userCollection.insertOne({
-      "username": "billtest",
-      "name": "Bill",
-      "email": "billtest@tasklocal.com"
-    });
-
-    print(await userCollection.find().toList());
   }
   static Future<String> insert(Mongodbmodel data) async {
     try {
