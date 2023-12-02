c554b640ec367ad1c060a06899c0b48a8067d5df
Richard Nguyen <81831944+richardn257@users.noreply.github.com>
2023-12-01 22:43:58 -0800

    Added an insert function to insert data

--- lib/Database/mongoconnection.dart
+++ lib/Database/mongoconnection.dart
@@ -1,4 +1,6 @@
 import 'dart:developer';
+import 'package:tasklocal/mongodbmodel.dart';
+
 import 'constants.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
@@ -10,5 +12,17 @@ class MongoConnection {
     await db.open();
     inspect(db);
     userCollection = db.collection(MONGO_TEST_COLLECTION);
+  }
+  static Future<String> insert(Mongodbmodel data) async {
+    try {
+      var result = await userCollection.insertOne(data.toJson());
+      if (result.isSuccess)
+        return "Data Inserted";
+      else
+        return "Something went wrong while inserting data";
+    } catch (e) {
+      print(e.toString());
+      return e.toString();
+    }  
   }
 }
