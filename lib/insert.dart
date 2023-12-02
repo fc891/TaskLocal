52e8c878a61d9351eb229ede11f391463de9262d
Richard Nguyen <81831944+richardn257@users.noreply.github.com>
2023-12-01 22:43:02 -0800

    Insert.dart can insert data to the database
    Add a function _insertData. Made the "Insert Data" button function.

--- lib/insert.dart
+++ lib/insert.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mongo_dart/mongo_dart.dart' as mongo_dart;
+import 'package:tasklocal/Database/mongoconnection.dart';
+import 'package:tasklocal/mongodbmodel.dart';
 
 class MongoDbInsert extends StatefulWidget {
   const MongoDbInsert({Key?key}) : super(key:key);
@@ -48,34 +51,52 @@ class MongoDbInsertState extends State<MongoDbInsert> {
               padding: const EdgeInsets.symmetric(horizontal: 50),
               child: TextField(
                 controller: addressController,
-                minLines: 3,
-                maxLines: 5,
+                // minLines: 3,
+                // maxLines: 5,
                 decoration: const InputDecoration(
-                  labelText: "Address Name"
+                  labelText: "Email Address"
                 ),
               )
             ),
-            const SizedBox(height: 50),
-            Row(
-              children: [
-                // OutlinedButton(
-                //   onPressed: () {
-                //   }, 
-                //   child: Text("")
-                // ),
-                ElevatedButton(
-                  onPressed: () {
-                    _insertData();
-                  }, 
-                  child: const Text("Insert Data")
-                )
-              ]
+            const SizedBox(height: 50),                
+            ElevatedButton(
+              onPressed: () {
+                _insertData(fnameController.text, lnameController.text, addressController.text);
+              }, 
+              child: const Text("Insert Data")
             )
+            // Row(
+            //   children: [
+            //     // OutlinedButton(
+            //     //   onPressed: () {
+            //     //   }, 
+            //     //   child: Text("")
+            //     // ),
+            //     ElevatedButton(
+            //       onPressed: () {
+            //         _insertData();
+            //       }, 
+            //       child: const Text("Insert Data")
+            //     )
+            //   ]
+            // )
           ],
         )
       )
     );
   }
 
-  Future<void> _insertData() async {}
+  Future<void> _insertData(String fname, String lname, String address) async {
+    var _id = mongo_dart.ObjectId();
+    final data = Mongodbmodel(id: _id, firstName: fname, lastName: lname, address: address);
+    var result = await MongoConnection.insert(data);
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Inserted ID " + _id.$oid)));
+    _clearAll();
+  }
+
+  void _clearAll() {
+    fnameController.text = '';
+    lnameController.text = '';
+    addressController.text = '';
+  }
 }
