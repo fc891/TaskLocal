ecee440cb16e51736f13cae40051b4daa8a0a6a0
Richard Nguyen <81831944+richardn257@users.noreply.github.com>
2023-12-02 22:26:21 -0800

    Added braces to if else & created getData()
    also assigned userCollection to the db collection

--- lib/Database/mongoconnection.dart
+++ lib/Database/mongoconnection.dart
@@ -11,14 +11,23 @@ class MongoConnection {
     db = await Db.create(MONGO_CONNECTION_URL);
     await db.open();
     inspect(db);
+    userCollection = db.collection(MONGO_TEST_COLLECTION);
   }
+
+  static Future<List<Map<String, dynamic>>> getData() async {
+    final arrData = await userCollection.find().toList();
+    return arrData;
+  }
+
   static Future<String> insert(Mongodbmodel data) async {
     try {
       var result = await userCollection.insertOne(data.toJson());
-      if (result.isSuccess)
+      if (result.isSuccess) {
         return "Data Inserted";
-      else
+      }
+      else {
         return "Something went wrong while inserting data";
+      }
     } catch (e) {
       print(e.toString());
       return e.toString();
