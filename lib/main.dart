743f41923d11d72df001929e8ee3d553d442a05a
Richard Nguyen <81831944+richardn257@users.noreply.github.com>
2024-01-31 18:48:30 -0800

    Revert "Removed anything related to MongoDB"
    This reverts commit 37216ea452cf362b5ee69051ffaba4e76fbdf6d6.

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: TaskerRegistration(),
+      home: CustomerRegistration(),
     );
   }
 }
