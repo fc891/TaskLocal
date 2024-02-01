140e14be906e88c7683681b447c187211d40f6ce
alexly01 <alexanderly01@gmail.com>
2024-02-01 11:32:32 -0800

    login page
    added loginpage as well as updated main and other files

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:tasklocal/Database/mongoconnection.dart';
-import 'package:tasklocal/Screens/taskerregistration.dart';
-import 'package:tasklocal/display.dart';
-import 'package:tasklocal/insert.dart';
-import 'package:tasklocal/Screens/customerregistration.dart';
+import 'Screens/loginpage.dart';
 
-
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await MongoConnection.connect(); //Connect to database
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
-      ),
-      // inserting data
-      //home: MongoDbInsert(),
-      // displaying data
-      //home: MongoDbDisplay(),
-      // customer registration screen
-      //home: CustomerRegistration()
-      // tasker registration screen
-      home: TaskerRegistration(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
