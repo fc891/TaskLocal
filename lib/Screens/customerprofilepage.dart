d14397a5da5e677f1873ddcba42e7a2f768dc182
fc891 <76578506+fc891@users.noreply.github.com>
2024-02-06 12:23:24 -0800

    Added more to customerprofilepage UI

--- lib/Screens/customerprofilepage.dart
+++ lib/Screens/customerprofilepage.dart
@@ -1,16 +1,27 @@
 // ignore_for_file: prefer_const_constructor
-import 'dart:ffi';
 
+import 'dart:ffi';
+import 'package:intl/intl.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class CustomerProfilePage extends StatelessWidget {
-  
+class CustomerProfilePage extends StatefulWidget {
+  const CustomerProfilePage({super.key});
+  @override
+  State<CustomerProfilePage> createState() => _CustomerProfilePageState();
+}
+
+class _CustomerProfilePageState extends State<CustomerProfilePage> {
+  String username = "Testlocal123";
+  String date = 'dd-MM-yyyy';
+  int requestscompleted = 0;
   //WIP
   //Get user's name using some sort of id (username?)
-  void getUserName(String id) async{
+  void getUserName(String id) async {
     var userId = id;
-    final snapshot = await FirebaseFirestore.instance.doc('customers/$userId').get();
+    username = "TaskLocal1";
+    final snapshot =
+        await FirebaseFirestore.instance.doc('customers/$userId').get();
     if (snapshot.exists) {
       print(snapshot);
     } else {
@@ -18,39 +29,99 @@ class CustomerProfilePage extends StatelessWidget {
     }
   }
 
+  //WIP
+  //Get user's join date using id
+  void getJoinDate(String id) async {
+    DateFormat joindateformat = DateFormat('dd-MM-yyyy');
+    DateTime joindate = DateTime(2024, 2, 6);
+    date = joindateformat.format(joindate);
+  }
+
+  //WIP
+  //Get user's number of requested tasks completed using id
+  void getRequestsCompleted(String id) async {
+    requestscompleted = 1;
+  }
+
+  //Run all getters above to initialize variables
+  void runGetters() {
+    String testid = "123";
+
+    getUserName(testid);
+    getJoinDate(testid);
+    getRequestsCompleted(testid);
+  }
+
   //Bill's Customer profile page screen/UI code
+  @override
   Widget build(BuildContext context) {
+    runGetters(); //Run all getter functions
     return Scaffold(
-      //Background color of UI
-      backgroundColor: Colors.green[500],
-      appBar: AppBar(
-          title: Text('User\'s profile page'),
-          centerTitle: true,
-          backgroundColor: Colors.green[800],
-          elevation: 0.0),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          children: <Widget>[
-            Text('USERNAME',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 1.0,
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold
-              )
-            ),
-            Text('USERNAME',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 1.0,
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold
-              )
-            ),
-          ]
-        )
-      )
-    );
+        //Background color of UI
+        backgroundColor: Colors.green[500],
+        appBar: AppBar(
+            title: Text('${username}\'s profile page'),
+            centerTitle: true,
+            backgroundColor: Colors.green[800],
+            elevation: 0.0),
+        body: Padding(
+            padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
+            child: Column(children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  child: Image.asset('lib/images/tasklocaltransparent.png'),
+                  radius: 40.0,
+                ),
+              ),
+              Center(
+                //Username text
+                child: Text('$username',
+                    style: TextStyle(
+                        color: Colors.white,
+                        letterSpacing: 1.0,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold)),
+              ),
+              Column(children: <Widget>[
+                Text('Join Date: $date',
+                    style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 1.0,
+                      fontSize: 16.0,
+                    )),
+                Text('Requested Tasks Completed: $requestscompleted',
+                    style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 1.0,
+                      fontSize: 16.0,
+                    )),
+              ]),
+              Divider(
+                height: 20.0,
+                color: Colors.grey[1500],
+              ),
+              Text('Request History',
+                  style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 1.3,
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.bold)),
+              Divider(
+                height: 20.0,
+                color: Colors.grey[1500],
+              ),
+              Expanded(
+                  child: SizedBox(
+                      height: 50.0,
+                      child: ListView.builder(
+                          itemCount: 20,
+                          itemBuilder: (context, index) {
+                            return Card(
+                                child: ListTile(
+                              onTap: () {},
+                              title: Text("test"),
+                            ));
+                          }))),
+            ])));
   }
 }
