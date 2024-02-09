1f42d67081d692e99e3ca68d1d227bb71b455800
Richard Nguyen <81831944+richardn257@users.noreply.github.com>
2024-02-08 17:37:49 -0800

    Changed screens folder to lowercase

--- lib/Screens/authorization/tasker_registration.dart
+++ lib/Screens/authorization/tasker_registration.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:tasklocal/Screens/tasker_home_page.dart';
+import 'package:tasklocal/screens/tasker_home_page.dart';
 
 // import 'package:tasklocal/Database/mongoconnection.dart';
 // import 'package:tasklocal/Database/mongodbmodelcustomer.dart';
