8f3323e90bba8b367ad18087839828f13fb72aef
Richard Nguyen <81831944+richardn257@users.noreply.github.com>
2024-02-08 18:14:27 -0800

    Made changes to the tasker_auth and tasker_login
    In tasker_auth, changed widget class name.
    In tasker_login, changed login method name, added app's image, added else if block for wrong email/pass, and changed the login button.

--- lib/screens/authorization/tasker_auth.dart
+++ lib/screens/authorization/tasker_auth.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:tasklocal/Screens/home_pages/tasker_home.dart';
-import 'taskerloginregister.dart';
+import '../tasker_home_page.dart';
+import 'tasker_login_register.dart';
 
-class AuthPage extends StatelessWidget {
-  const AuthPage({super.key});
+// auth page 
+class TaskerAuthPage extends StatelessWidget {
+  const TaskerAuthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
