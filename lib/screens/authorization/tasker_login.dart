ee7bcd01d119b8605d594fed811195b838bf4f31
Richard Nguyen <81831944+richardn257@users.noreply.github.com>
2024-02-11 23:11:37 -0800

    Added comments and decorate the screen

--- lib/screens/authorization/tasker_login.dart
+++ lib/screens/authorization/tasker_login.dart
@@ -13,9 +13,9 @@ class _TaskerLogin extends State<TaskerLogin> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
-  // login method
+  // login method called on when press the login button
   void userLogin() async {
-    // show loading circle
+    // display a loading circle to give users idea of time
     showDialog(
       context: context, 
       builder: (context) {
@@ -25,25 +25,23 @@ class _TaskerLogin extends State<TaskerLogin> {
       }
     );
 
-    // try sign in
     try {
+      // verify whether the account is stored in the database and then sign in
       await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: emailController.text, 
         password: passwordController.text,
       );
-      // pop the loading circle
+      // remove the loading circle after logging in
       Navigator.pop(context);   
 
     } on FirebaseAuthException catch (e) {
-      // pop the loading circle
       Navigator.pop(context);
+      // get an idea of what the 'logging in' error is
       print(e.code);
-      // wrong email
+      // Display the error message depending on the error code
       if (e.code == 'invalid-email' || e.code == 'user-not-found') {
-        // show error to user
         showErrorMessage('Incorrect Email');
       } 
-      // wrong password
       else if (e.code == 'wrong-password') {
         showErrorMessage("Incorrect Password");
       }
@@ -53,7 +51,7 @@ class _TaskerLogin extends State<TaskerLogin> {
     }
   }
 
-  // error message to user
+  // convenient method to display error message
   void showErrorMessage(String message) {
     showDialog(
       context: context,
@@ -75,43 +73,65 @@ class _TaskerLogin extends State<TaskerLogin> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                // const Padding(
-                //   padding: EdgeInsets.only(top: 30),
-                // ),
+                // app's logo image
                 Align(
-                  alignment: Alignment.topCenter,
-                  child: Image.asset('lib/images/tasklocaltransparent.png'),
-                ),
-                const SizedBox(height: 0),
-                Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 50),
-                    child: TextField(
-                      controller: emailController,
-                      decoration: const InputDecoration(labelText: "Email Address"),
-                    )),
-                Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 50),
-                    child: TextField(
-                      controller: passwordController,
-                      decoration: const InputDecoration(labelText: "Password"),
-                      obscureText: true,
-                    )),
-                const SizedBox(height: 20),
-                // ElevatedButton(
-                //   onPressed: () {
-                //     userLogin();
-                //   }, 
-                //   child: const Text("Login"),
-                // ),
-                Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Center(
-                  child: ElevatedButton(
-                    onPressed: userLogin,
-                    child: Text("Login"),
+                  alignment: Alignment.topCenter, // Aligns to the top center
+                  child: Image.asset(
+                    'lib/images/tasklocaltransparent.png',
+                    height: 300,
                   ),
                 ),
-              ),
+                // title of the login page
+                Text(
+                  'Tasker Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: "Email Address", 
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[250],
+                    ),
+                  )
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: TextField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      labelText: "Password", 
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[250],
+                    ),
+                    obscureText: true,
+                  )
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Center(
+                    child: ElevatedButton(
+                      onPressed: userLogin,
+                      child: Text("Login"),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -124,8 +144,8 @@ class _TaskerLogin extends State<TaskerLogin> {
                         style: TextStyle(
                           color: Colors.yellow, 
                           fontWeight: FontWeight.bold
-                        )
-                      )
+                        ),
+                      ),
                     ),
                   ],
                 ),
