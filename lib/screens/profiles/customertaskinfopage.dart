f8fd12f0d285be69fc32d87ec6b97cf66dddc8ac
fc891 <76578506+fc891@users.noreply.github.com>
2024-02-13 12:30:20 -0800

    Updated Customer Profile Page
    -Added functionality to task history buttons
    -Made a new class to save task info
    -Updated pubspec

---
+++ lib/screens/profiles/customertaskinfopage.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_const_constructor
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tasklocal/screens/profiles/taskinfo.dart';
+
+class CustomerTaskInfoPage extends StatelessWidget {
+  const CustomerTaskInfoPage({super.key, required this.taskinfo});
+  final TaskInfo taskinfo;
+
+//Customer task info screen
+  @override
+  Widget build(BuildContext context) {
+    String taskcategory = taskinfo.taskCategory;
+    int tasknumber = taskinfo.taskNumber;
+    return Scaffold(
+      //Background color of UI
+      backgroundColor: Colors.green[500],
+      //UI Appbar (bar at top of screen)
+      appBar: AppBar(
+        title: Text('Task#$tasknumber'),
+        centerTitle: true,
+        backgroundColor: Colors.green[800],
+        elevation: 0.0,
+      ),
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+          child: Column(children: [
+        const Padding(
+          padding: EdgeInsets.only(top: 30, bottom: 20),
+          // child: Text(
+          //   "",
+          //   style: TextStyle(fontSize: 20),
+          // ),
+        ),
+      ])),
+    );
+  }
+}
