d7f13d48563abecd6aa7c2e12854f05c82dcd09e
Richard Nguyen <81831944+richardn257@users.noreply.github.com>
2024-03-20 17:15:39 -0700

    Update sign_up_task.dart

--- lib/screens/sign_up_task/sign_up_task.dart
+++ lib/screens/sign_up_task/sign_up_task.dart
@@ -37,214 +37,246 @@ class _SignUpTaskState extends State<SignUpTask> {
         backgroundColor: Colors.green[800],
       ),
       body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "How many experience do you have?",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // get the user's address info
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Enter address you'll work around",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: TextField(
-                controller: expController,
-                decoration: InputDecoration(
-                  hintText: 'Enter the amount',
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  // border is black by default and when click the text field, border is white
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black), 
-                    borderRadius: BorderRadius.circular(10)
-                  )
-                ),
-              ),
-            ),
-            SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "How many experience do you have?",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: TextField(
+                  controller: expController,
+                  decoration: InputDecoration(
+                    hintText: 'Address, zip code, state',
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    // border is black by default and when click the text field, border is white
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black), 
+                      borderRadius: BorderRadius.circular(10)
+                    )
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: TextField(
-                controller: expController,
-                decoration: InputDecoration(
-                  hintText: 'Enter the amount',
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  // border is black by default and when click the text field, border is white
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black), 
-                    borderRadius: BorderRadius.circular(10)
-                  )
-                ),
-              ),
-            ),
-            SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "How many experience do you have?",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+              SizedBox(height: 15),
+              // ask the user for the asking rate
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "What is your asking rate?",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: TextField(
-                      controller: expController,
-                      decoration: InputDecoration(
-                        hintText: 'Enter the amount',
-                        filled: true,
-                        fillColor: Colors.grey[200],
-                        // border is black by default and when click the text field, border is white
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                          borderRadius: BorderRadius.circular(10.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: TextField(
+                  controller: expController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter the amount',
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    // border is black by default and when click the text field, border is white
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black), 
+                      borderRadius: BorderRadius.circular(10)
+                    )
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              // Ask and retrieve the user's experience
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "How many experience do you have?",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        controller: expController,
+                        decoration: InputDecoration(
+                          hintText: 'Enter the amount',
+                          filled: true,
+                          fillColor: Colors.grey[200],
+                          // border is black by default and when click the text field, border is white
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black), 
+                            borderRadius: BorderRadius.circular(10)
+                          )
                         ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black), 
-                          borderRadius: BorderRadius.circular(10)
-                        )
                       ),
                     ),
                   ),
-                ),
-                SizedBox(width: 10),
-                Padding(
-                  padding: const EdgeInsets.only(right: 20.0),
-                  child: DropdownButton<String>(
-                    value: dropdownValue,
-                    icon: Icon(Icons.arrow_drop_down),
-                    iconSize: 24,
-                    elevation: 16,
-                    style: TextStyle(color: Colors.black),
-                    underline: Container(
-                      height: 2,
-                      color: Colors.grey[200],
-                    ),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        dropdownValue = newValue!;
-                      });
-                    },
-                    items: <String>['Years', 'Months', 'Weeks']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20), // Add some space between the experience section and skills section
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Skills:",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+                  SizedBox(width: 10),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20.0),
+                    child: DropdownButton<String>(
+                      value: dropdownValue,
+                      icon: Icon(Icons.arrow_drop_down),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: TextStyle(color: Colors.black),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.grey[200],
                       ),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                        });
+                      },
+                      items: <String>['Years', 'Months', 'Weeks']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
                     ),
-                    SizedBox(height: 8),
-                    SizedBox(
-                      width: 150,
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: skills.length + 1, // +1 for the initial empty text field
-                        itemBuilder: (context, index) {
-                          if (index == skills.length) {
-                            // Add button to add more skills
-                            return TextButton(
-                              onPressed: () {
-                                setState(() {
-                                  skills.add('');
-                                });
-                              },
-                              child: Text(
-                                "Add Skill",
-                                style: TextStyle(color: Colors.white),
-                              ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              // skills section where users can input their skills
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Skills:",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        height: 300,
+                        width: 300,
+                        color: Colors.green[400],
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: skills.length + 1, // +1 for the initial empty text field
+                          itemBuilder: (context, index) {
+                            if (index == skills.length) {
+                              // Add button to add more skills
+                              return IconButton(
+                                icon: Icon(Icons.add),
+                                color: Colors.white,
+                                onPressed: () {
+                                  setState(() {
+                                    skills.add('');
+                                  });
+                                },
+                              );
+                            }
+                            return Row(
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: TextFormField(
+                                      style: TextStyle(color: Colors.black),
+                                      decoration: InputDecoration(
+                                        hintText: "Enter skill ${index + 1}",
+                                        hintStyle: TextStyle(color: Colors.grey),
+                                        filled: true,
+                                        fillColor: Colors.grey[200],
+                                        focusedBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(color: Colors.white),
+                                          borderRadius: BorderRadius.circular(10),
+                                        ),
+                                        enabledBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(color: Colors.black),
+                                          borderRadius: BorderRadius.circular(10),
+                                        ),
+                                      ),
+                                      onChanged: (value) {
+                                        setState(() {
+                                          skills[index] = value;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.delete),
+                                  onPressed: () {
+                                    setState(() {
+                                      skills.removeAt(index);
+                                    });
+                                  },
+                                ),
+                              ],
                             );
-                          }
-                          return TextFormField(
-                            // initialValue: skills[index],
-                            // onChanged: (value) {
-                            //   setState(() {
-                            //     skills[index] = value;
-                            //   });
-                            // },
-                            style: TextStyle(color: Colors.black),
-                            decoration: InputDecoration(
-                              hintText: "Enter skill ${index + 1}",
-                              hintStyle: TextStyle(color: Colors.grey),
-                              filled: true,
-                              fillColor: Colors.grey[200],
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.white),
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.black),
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                          );
-                        },
+                          },
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green[800],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 15),
+                  child: Text("Submit", style: TextStyle(color: Colors.white, fontSize: 16)),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
