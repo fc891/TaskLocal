3883566a050d9e5238af0a93602616e67668f8a5
Richard Nguyen <81831944+richardn257@users.noreply.github.com>
2024-04-24 01:18:05 -0700

    retrieve comments and navigate to DiscussionPage
    Tasker can view their comments and navigate to the DiscussionPage to delete their comments (discussion topic post can be deleted if commented on there).

--- lib/screens/discussion_board/posted_comments_history.dart
+++ lib/screens/discussion_board/posted_comments_history.dart
@@ -23,7 +23,7 @@ class _PostedCommentsHistoryState extends State<PostedCommentsHistory> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: FutureBuilder<List<DocumentSnapshot>>(
-          future: _getPostedTopics(sortBy),
+          future: _getPostedComments(sortBy),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return Center(
@@ -86,128 +86,81 @@ class _PostedCommentsHistoryState extends State<PostedCommentsHistory> {
                       itemCount: topics.length,
                       itemBuilder: (context, index) {
                         final topic = topics[index];
-                        final topicPosterEmail = topic['email'];
-                        final taskCategory = topic['task category'];
-                        final topicTitle = topic['topic title'];
+                        // final topicPosterEmail = topic['email'];
+                        // final taskCategory = topic['task category'];
+                        // final topicTitle = topic['topic title'];
                         final text = topic['text'];
-                        final mmddyy = topic['formatted date'];
+                        // final mmddyy = topic['formatted date'];
                         final time = topic['time'];
                         final username = topic['username'];
-                        final numOfMsg = topic['num of msg'];
-                        final usersLiked = topic['liked by users'];
+                        // final numOfMsg = topic['num of msg'];
+                        // final usersLiked = topic['liked by users'];
                         final timeWithSeconds = topic['time with seconds'];
+                        // final locationOfTopicTitleDoc = topic['location of topic title doc'];
                         // Check if the current user has liked the topic
-                        final List<dynamic> likedByUsers = topic['liked by users'] ?? [];
+                        // final List<dynamic> likedByUsers = topic['liked by users'] ?? [];
                         final currentUserEmail = _auth.currentUser!.email;
-                        final isLiked = likedByUsers.contains(currentUserEmail);
+                        // final isLiked = likedByUsers.contains(currentUserEmail);
                     
                         final date = topic['date'].toDate();
                         // final DateFormat formatter = DateFormat('MM/dd/yyyy');
                         final String formattedDate = DateFormat('MM/dd/yyyy').format(date);
                         // final String time = DateFormat('h:mm a').format(date);
+
+                        // final postedTopicData = _firestore.collection('Tasker Discussion Board').doc(topic['topic poster email']).collection('Posted Topics').doc(topic['posted topic location']).get();
+                        // _getPostTopicData(topic['topic poster email'], topic['posted topic location']);
+
+                        final topicPosterEmail = topic['topic poster email'];
+                        final postedTopicLocation = topic['posted topic location'];
                     
-                        return GestureDetector(
-                            onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => DiscussionPage(topicPosterEmail: topicPosterEmail, taskCategory: taskCategory, topicTitle: topicTitle, text: text, username: username,
-                                                                                      numOfMsg: numOfMsg, usersLiked: usersLiked, mmddyy: mmddyy, time: time, timeWithSeconds: timeWithSeconds, 
-                                                                                      onLikeUpdated: () {
-                                    // Trigger rebuild when like is updated
-                                    setState(() {});
-
-                                    // Trigger rebuild when like is updated
-                                            if (widget.onLikeUpdated != null) {
-                                              widget.onLikeUpdated();
-                                            }
-                                  }, isTextFieldVisible: false)),
-                            ).then((updatedData) {                    
-                              if (updatedData) {
-                                setState(() {});
-                              }
-                            });
-                          },
-                          child: ListTile(
-                            contentPadding: EdgeInsets.all(0),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  children: [
-                                    Text('$taskCategory'),
-                                    Spacer(),
-                                    GestureDetector(
-                                      onTap: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(builder: (context) => DiscussionPage(topicPosterEmail: topicPosterEmail, taskCategory: taskCategory, topicTitle: topicTitle, text: text, username: username,
-                                                                                                  numOfMsg: numOfMsg, usersLiked: usersLiked, mmddyy: mmddyy, time: time, timeWithSeconds: timeWithSeconds,
-                                                                                                  onLikeUpdated: () {
-                                                // Trigger rebuild when like is updated
-                                                setState(() {});
-
-                                                // Trigger rebuild when like is updated
-                                            if (widget.onLikeUpdated != null) {
-                                              widget.onLikeUpdated();
-                                            }
-
-                                              }, isTextFieldVisible: true)),
-                                        ).then((updatedData) {                    
-                                          if (updatedData) {
-                                            setState(() {});
-                                          }
-                                        });
-                                      },
-                                      child: Icon(
-                                        Icons.message, 
-                                        color: Theme.of(context).colorScheme.secondary
-                                      )
-                                    ),
-                                    Text('$numOfMsg'),
-                                    GestureDetector(
-                                      onTap: () async {
-                                        setState(() {});
-                                        // Access the document reference
-                                        final documentReference = topic.reference;
-                    
-                                        // List of users who liked the topic, stored as 'liked by users'
-                                        final List<dynamic> likedByUsers = topic['liked by users'] ?? [];
-                    
-                                        // Check if the current user's email is in the list of likedByUsers
-                                        final currentUserEmail = _auth.currentUser!.email;
-                                        if (isLiked) {
-                                          // If the user unlikes the topic, remove their email from the list
-                                          likedByUsers.remove(currentUserEmail);
-                                        } else {
-                                          // If the user likes the topic, add their email to the list
-                                          likedByUsers.add(currentUserEmail);
-                                        }
-                    
-                                        // Update the 'liked by users' field in Firestore
-                                        await documentReference.update({
-                                          'liked by users': likedByUsers,
-                                        });
-
-                                            // Trigger rebuild when like is updated
-                                            if (widget.onLikeUpdated != null) {
-                                              widget.onLikeUpdated();
-                                            }
-
-
-                                      },
-                                      child: Icon(
-                                        isLiked ? Icons.favorite : Icons.favorite_border, 
-                                        color: isLiked ? Colors.red : Theme.of(context).colorScheme.secondary
-                                      ),
-                                    ),
-                                    Text(likedByUsers.length.toString()),
-                                  ],
+                        return FutureBuilder<DocumentSnapshot>(
+                          future: _getPostTopicData(topicPosterEmail, postedTopicLocation),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState == ConnectionState.waiting) {
+                              return CircularProgressIndicator(); // Replace with a loading indicator
+                            } else if (snapshot.hasError) {
+                              return Text('Error: ${snapshot.error}');
+                            } else {
+                              final postedTopicData = snapshot.data!;
+                              return GestureDetector(
+                                onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => DiscussionPage(topicPosterEmail: postedTopicData['email'], taskCategory: postedTopicData['task category'], topicTitle: postedTopicData['topic title'], 
+                                                                                          text: postedTopicData['text'], username: postedTopicData['username'],
+                                                                                          numOfMsg: postedTopicData['num of msg'], usersLiked: postedTopicData['liked by users'], mmddyy: postedTopicData['formatted date'], 
+                                                                                          time: postedTopicData['time'], timeWithSeconds: postedTopicData['time with seconds'], 
+                                                                                          onLikeUpdated: () {
+                                          // Trigger rebuild when like is updated
+                                          setState(() {});
+                              
+                                          // Trigger rebuild when like is updated
+                                                  if (widget.onLikeUpdated != null) {
+                                                    widget.onLikeUpdated();
+                                                  }
+                                        }, isTextFieldVisible: false)),
+                                  ).then((updatedData) {                    
+                                    if (updatedData) {
+                                      setState(() {});
+                                    }
+                                  });
+                                },
+                                child: ListTile(
+                                  contentPadding: EdgeInsets.all(0),
+                                  subtitle: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text('${postedTopicData['task category']}'),
+                                      Text('${postedTopicData['topic title']}'),
+                                      Text('Commented:'),
+                                      Text('$text'),
+                                      Text('$formattedDate $time'),
+                                    ],
+                                  ),
                                 ),
-                                Text('$topicTitle'),
-                                Text('$formattedDate $time'),
-                              ],
-                            ),
-                          ),
+                              );
+                            }
+                          },
                         );
                       },
                     ),
@@ -221,28 +174,126 @@ class _PostedCommentsHistoryState extends State<PostedCommentsHistory> {
     );
   }
 
-  Future<List<DocumentSnapshot>> _getPostedTopics(String sortBy) async {
-    // Access the collection that stores user's signed up tasks
-    final postedTopicsCollection = await _firestore.collection('Tasker Discussion Board').doc(_auth.currentUser!.email)
-                          .collection('Posted Topics').get();
-                          
-    // List to store the documents in the Posted Topics subcollection
-    List<DocumentSnapshot> postedTopics = postedTopicsCollection.docs;
+  // Define a function to fetch data for a single posted topic
+  Future<DocumentSnapshot> _getPostTopicData(String topicPosterEmail, String postedTopicLocation) async {
+    final postedTopicData = await _firestore
+      .collection('Tasker Discussion Board')
+      .doc(topicPosterEmail)
+      .collection('Posted Topics')
+      .doc(postedTopicLocation)
+      .get();
+    return postedTopicData;
+  }
+
+  Future<List<DocumentSnapshot>> _getPostedComments(String sortBy) async {
+    final commentedTopicsCollection = await _firestore.collection('Taskers').doc(_auth.currentUser!.email).collection('Commented Topics').get();
+    final List<DocumentSnapshot> commentedTopicsList = [];
+    final Set<String> uniqueComments = {}; // Use a set to store unique comments
+
+    for (final commentedTopic in commentedTopicsCollection.docs) {
+      final commentedData = commentedTopic.data();
+      final topicPosterEmail = commentedData['topic poster email'];
+      final locationOfCommentDoc = commentedData['posted topic location'];
+
+      final postedTopics = _firestore.collection('Tasker Discussion Board').doc(topicPosterEmail);
+      final tasker = await postedTopics.collection('Posted Topics').doc(locationOfCommentDoc).collection('Comments').where('email', isEqualTo: _auth.currentUser!.email).get();
+
+      // commentedTopicsList.addAll(tasker.docs);
+      for (final doc in tasker.docs) {
+        final commentId = doc.id; // Assuming each comment has a unique ID
+        if (!uniqueComments.contains(commentId)) { // Check if the comment is unique
+          uniqueComments.add(commentId); // Add the comment ID to the set
+          commentedTopicsList.add(doc); // Add the comment to the list
+        }
+      }
+      // print(commentedTopicsList);
+    }
 
     // Sort comments based on selected value
     if (sortBy == 'New') {
-      postedTopics.sort((a, b) {
+      commentedTopicsList.sort((a, b) {
         DateTime dateA = a['date'].toDate();
         DateTime dateB = b['date'].toDate();
         return dateB.compareTo(dateA); // Sort in descending order (latest first)
       });
     } else if (sortBy == 'Old') {
-      postedTopics.sort((a, b) {
+      commentedTopicsList.sort((a, b) {
         DateTime dateA = a['date'].toDate();
         DateTime dateB = b['date'].toDate();
         return dateA.compareTo(dateB); // Sort in ascending order (oldest first)
       });
     }
-    return postedTopics;
+
+    // for (DocumentSnapshot doc in commentedTopicsList) {
+    //   print(doc.data()); // Print entire data of the document
+    // }
+
+    return commentedTopicsList;
   }
+
+
+  // Future<List<DocumentSnapshot>> _getPostedTopics(String sortBy) async {
+  //   // Access the collection that stores user's signed up tasks
+  //   final postedTopicsCollection = await _firestore.collection('Tasker Discussion Board').doc(_auth.currentUser!.email)
+  //                         .collection('Posted Topics').get();
+                          
+  //   // List to store the documents in the Posted Topics subcollection
+  //   List<DocumentSnapshot> postedTopics = postedTopicsCollection.docs;
+
+  //   // Sort comments based on selected value
+  //   if (sortBy == 'New') {
+  //     postedTopics.sort((a, b) {
+  //       DateTime dateA = a['date'].toDate();
+  //       DateTime dateB = b['date'].toDate();
+  //       return dateB.compareTo(dateA); // Sort in descending order (latest first)
+  //     });
+  //   } else if (sortBy == 'Old') {
+  //     postedTopics.sort((a, b) {
+  //       DateTime dateA = a['date'].toDate();
+  //       DateTime dateB = b['date'].toDate();
+  //       return dateA.compareTo(dateB); // Sort in ascending order (oldest first)
+  //     });
+  //   }
+  //   return postedTopics;
+  // }
+
+  // // retrieves the comments that the user made on posted topics
+  // Future<Map<String, List<DocumentSnapshot>>> _getPostedComments(String sortBy) async {
+  //   // access the collection that stores the users comments
+  //   final commentedTopicsCollection = await _firestore.collection('Taskers').doc(_auth.currentUser!.email).collection('Commented Topics').get();
+  //   final commentedTopicsList = <String, List<DocumentSnapshot>>{};
+
+  //   // go through the Commented Topics collection to view each comments
+  //   for (final commentedTopic in commentedTopicsCollection.docs) {
+  //     final commentedData = commentedTopic.data();
+  //     final topicPosterEmail = commentedData['topic poster email'];
+  //     final locationOfCommentDoc = commentedData['location of comments doc'];
+  //     // retrieve posted topic from specific poster
+  //     final postedTopics = _firestore.collection('Tasker Discussion Board').doc(topicPosterEmail);
+  //     // go to the Comments collection from the Posted Topics collection
+  //     final tasker = await postedTopics.collection('Posted Topics').doc(locationOfCommentDoc).collection('Comments').where('email', isEqualTo: _auth.currentUser!.email).get();
+  //     // if categroy name is not stored in list, then create the key and assign it an empty list.
+  //     if (!commentedTopicsList.containsKey(locationOfCommentDoc)) {
+  //       commentedTopicsList[locationOfCommentDoc] = [];
+  //     }
+  //     commentedTopicsList[locationOfCommentDoc]!.addAll(tasker.docs);
+  //   }
+  //   // Sort comments based on selected value
+  //   commentedTopicsList.forEach((key, value) {
+  //     if (sortBy == 'New') {
+  //       value.sort((a, b) {
+  //         DateTime dateA = a['date'].toDate();
+  //         DateTime dateB = b['date'].toDate();
+  //         return dateB.compareTo(dateA); // Sort in descending order (latest first)
+  //       });
+  //     } else if (sortBy == 'Old') {
+  //       value.sort((a, b) {
+  //         DateTime dateA = a['date'].toDate();
+  //         DateTime dateB = b['date'].toDate();
+  //         return dateA.compareTo(dateB); // Sort in ascending order (oldest first)
+  //       });
+  //     }
+  //   });
+  //   return commentedTopicsList;
+  // }
 }
