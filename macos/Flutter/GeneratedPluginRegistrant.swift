ffa3ab653a339a8c7e2cd882b60ed2349f34af04
fc891 <76578506+fc891@users.noreply.github.com>
2024-04-27 17:06:20 -0700

    Finished routing and tracking for GPS/Map view
    -Added tracking for both customer and tasker
    -Added routing line for both customer and tasker
    -Updated the android kotlin version to most recent (1.7.10 -> 1.9.23) to use the Flutter Location library

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,7 @@ import firebase_storage
 import flutter_local_notifications
 import flutter_timezone
 import geolocator_apple
+import location
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
@@ -29,6 +30,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
