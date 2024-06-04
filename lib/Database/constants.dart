05c404ca11a604e773127dccf45a585b52aa789b
fc891 <76578506+fc891@users.noreply.github.com>
2024-06-03 21:54:48 -0700

    Update constants.dart

--- lib/Database/constants.dart
+++ lib/Database/constants.dart
@@ -1,13 +1,1 @@
-// const MONGO_CONNECTION_URL = "mongodb+srv://test_user:fkgSzO9Ox62v3zNI@tasklocal.y7njlqc.mongodb.net/"; // Database connection url for test_user
-const MONGO_CONNECTION_URL = "mongodb+srv://test_user:fkgSzO9Ox62v3zNI@tasklocal.y7njlqc.mongodb.net/TaskLocalTest?retryWrites=true&w=majority";
-const MONGO_TEST_COLLECTION = "test"; //Database collection name
-
-const MONGO_CUSTOMER_COLLECTION = "customer"; //Customer database/collection name
-const MONGO_TASKER_COLLECTION = "tasker"; //Tasker database/collection name
-
-//Test user details:
-//username: test_user
-//password: fkgSzO9Ox62v3zNI
-
-//Connection url (replace <username> and <password>, "< and > included"):
-//mongodb+srv://<username>:<password>@tasklocal.y7njlqc.mongodb.net/
+//Removed
